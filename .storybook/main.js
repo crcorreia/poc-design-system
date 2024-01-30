b4e1d2ee9cd1e75d5f464025ebf17de37538ad19
Correia <cesar.correia@capgemini.com>
2024-01-30 11:36:46 +0000

    Changelog for v0.1.35 [skip ci]

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,13 @@
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    //'../src/Intro.mdx',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
